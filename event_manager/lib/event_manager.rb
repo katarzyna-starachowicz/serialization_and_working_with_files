744f2075408566d30ed684633bae112dbbf70858
Katarzyna Starachowicz <kstarachowicz@gmail.com>
2016-01-13 13:36:55 +0100

    using Sunlight

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,5 +1,9 @@
 #http://tutorials.jumpstartlab.com/projects/eventmanager.html
 require "csv"
+require "sunlight/congress"
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
 
   # if the zip code has not been specified, create a "00000" pseudo-zipcode
   # if the zip code is exactly five digits, assume that it is ok
@@ -9,14 +13,26 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
 
-puts "EventManager Initialized!"
+def legislators_by_zipcode(zipcode)
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+  
+  legislator_names = legislators.collect do |legislator|
+    "#{legislator.first_name} #{legislator.last_name}"
+  end
+
+  legislator_names.join(", ")
+end
+
+puts "\nEventManager Initialized.\n"
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
 contents.each do |row|
   name = row[:first_name]
+
   zipcode = row[:zipcode]
 
-  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode}"
+  puts "#{name} #{zipcode} #{legislators}"
 end
