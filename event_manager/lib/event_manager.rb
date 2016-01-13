669eb2e3fc0b41b84713ccd5836be17cde3008f5
Katarzyna Starachowicz <kstarachowicz@gmail.com>
2016-01-13 12:39:17 +0100

    parsing with csv, cleaning up zipcode

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,9 +1,22 @@
+#http://tutorials.jumpstartlab.com/projects/eventmanager.html
+require "csv"
+
+  # if the zip code has not been specified, create a "00000" pseudo-zipcode
+  # if the zip code is exactly five digits, assume that it is ok
+  # if the zip code is more than 5 digits, truncate it to the first 5 digits
+  # if the zip code is less than 5 digits, add zeros to the front until it becomes five digits
+def clean_zipcode(zipcode)  
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
+
 puts "EventManager Initialized!"
 
-lines = File.readlines "event_attendees.csv"
-lines.each_with_index do |line, index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
-  puts name
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  puts "#{name} #{zipcode}"
 end
