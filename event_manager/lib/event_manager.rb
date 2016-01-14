44241ac35df163fef04e2b7558c646033d210787
Katarzyna Starachowicz <kstarachowicz@gmail.com>
2016-01-14 12:42:55 +0100

    debug phone stuff

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -5,7 +5,7 @@ require 'erb'
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
 def clean_phone_number(phone_number)
-  pn = phone_number.split("").select{|i| i =~ /\d/}.join("")
+  pn = phone_number.scan(/\d/).join("")
   if (pn.length == 10) || (pn.length == 11  && pn[0] == "1")
     pn[-10..-1]
   else
@@ -31,6 +31,12 @@ def save_thank_you_letters(id,form_letter)
   end
 end
 
+def save_number_phones(name, homephone)
+  Dir.mkdir("phones") unless Dir.exists?("phones")
+
+  File.open("phones/phones.txt", 'a') { |file| file.puts "#{name}: #{homephone}" }
+end
+
 puts "EventManager initialized."
 
 contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
@@ -42,10 +48,11 @@ contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
-  phone_number = clean_phone_number(row[:homephone])
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
-
   save_thank_you_letters(id,form_letter)
+
+  homephone = clean_phone_number(row[:homephone])
+  save_number_phones(name, homephone)
 end
