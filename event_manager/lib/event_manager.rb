fda9a6a454ad49bb1636a4028217d0017c38c775
Katarzyna Starachowicz <kstarachowicz@gmail.com>
2016-01-14 10:16:20 +0100

    form letters

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,38 +1,41 @@
-#http://tutorials.jumpstartlab.com/projects/eventmanager.html
-require "csv"
-require "sunlight/congress"
+require 'csv'
+require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
-
-  # if the zip code has not been specified, create a "00000" pseudo-zipcode
-  # if the zip code is exactly five digits, assume that it is ok
-  # if the zip code is more than 5 digits, truncate it to the first 5 digits
-  # if the zip code is less than 5 digits, add zeros to the front until it becomes five digits
-def clean_zipcode(zipcode)  
-  zipcode.to_s.rjust(5, "0")[0..4]
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
 end
 
 def legislators_by_zipcode(zipcode)
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-  
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
-  end
-
-  legislator_names.join(", ")
+  Sunlight::Congress::Legislator.by_zipcode(zipcode)
 end
 
-puts "\nEventManager Initialized.\n"
+def save_thank_you_letters(id,form_letter)
+  Dir.mkdir("output") unless Dir.exists?("output")
 
-contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename,'w') do |file|
+    file.puts form_letter
+  end
+end
+
+puts "EventManager initialized."
+
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-
-  zipcode = row[:zipcode]
-
+  zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letters(id,form_letter)
 end
